9f84f5b9224aaf29f6049b52ebd5228aefde0747
Eleanor Burgess <Eleanor.C.Burgess0940@gmail.com>
2020-03-20 13:02:13 -0400

    added functionality to timer and a basic set up for questions

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-var timeEl = document.querySelector(".time");
-
-timeEl.addEventListener("click", function () {
-
-});
+var startBtn = document.getElementById("startBtn");
+var secondsLeft = (90);
+var timer = document.getElementById("timer")
 
 
+
+function startTimer() {
+    setTimer();
+}
+
+function setTimer() {
+    var countdown = setInterval(function () {
+        secondsLeft--;
+        timer.textContent = "time left:" + secondsLeft;
+
+        if (secondsLeft === 0) {
+            clearInterval(countdown);
+        }
+    }, 1000);
+}
+startBtn.addEventListener("click", startTimer);
---
+++ questions.js
@@ -0,0 +1,18 @@
+var questions = [
+    {
+        question: "question?",
+        choiceA: "a",
+        choiceB: "b",
+        choiceC: "c",
+        choiceD: "d",
+        correct: "A",
+    }
+    {
+        question: "question?",
+        choiceA: "a",
+        choiceB: "b",
+        choiceC: "c",
+        choiceD: "d",
+        correct: "A",
+    }
+];
