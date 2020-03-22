921f4e83a198854f19941be2774f553853bd90d6
Eleanor Burgess <Eleanor.C.Burgess0940@gmail.com>
2020-03-22 14:20:06 -0400

    added javascript to change the page from home page to quiz page when the timer starts

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 var startBtn = document.getElementById("startBtn");
 var secondsLeft = (90);
-var timer = document.getElementById("timer")
+var timer = document.getElementById("timer");
 
 
 
 function startTimer() {
+    document.getElementById("homePage").classList.add("d-none")
+    document.getElementById("quizPage").classList.remove("d-none")
     setTimer();
 }
 
