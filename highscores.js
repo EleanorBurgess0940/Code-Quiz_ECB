ac52d95fda8ac282c62fb47d15ccaf627b4e6afd
Eleanor Burgess <Eleanor.C.Burgess0940@gmail.com>
2020-03-23 00:25:29 -0400

    added comments

--- highscores.js
+++ highscores.js
@@ -1,20 +1,21 @@
 var clearBtn = document.getElementById("clearBtn");
 var submitBtn = document.getElementById("submitBtn");
 
-
+//local storage
 highScores = JSON.parse(localStorage.getItem("highScores") || "[]"),
     highScoreList = document.getElementById("highScoreList");
 highScores.sort(function (low, high) {
     return high.score - low.score
 })
 
+//this function makes and sorts the high scores
 for (var scoreNum = 0; scoreNum < highScores.length; scoreNum++) {
     var newLi = document.createElement("li")
     newLi.textContent = highScores[scoreNum].name + " - " + highScores[scoreNum].score
     highScoreList.appendChild(newLi)
 }
 
-
+//this function clears the high scores
 clearBtn.addEventListener("click", function () {
     localStorage.clear();
     location.reload();
