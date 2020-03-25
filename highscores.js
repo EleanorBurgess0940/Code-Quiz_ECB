e6cb56ed53a64307dd930a7c7c246db5b2967563
Eleanor Burgess <Eleanor.C.Burgess0940@gmail.com>
2020-03-25 00:42:11 -0400

    added individuality to javascript and worked out a different way of doing things

--- highscores.js
+++ highscores.js
@@ -1,23 +1,34 @@
-var clearBtn = document.getElementById("clearBtn");
 var submitBtn = document.getElementById("submitBtn");
+var clearBtn = document.getElementById("clearBtn");
 
-//local storage
-highScores = JSON.parse(localStorage.getItem("highScores") || "[]"),
-    highScoreList = document.getElementById("highScoreList");
-highScores.sort(function (low, high) {
-    return high.score - low.score
-})
-
-//this function makes and sorts the high scores
-for (var scoreNum = 0; scoreNum < highScores.length; scoreNum++) {
-    var newLi = document.createElement("li")
-    newLi.textContent = highScores[scoreNum].name + " - " + highScores[scoreNum].score
-    highScoreList.appendChild(newLi)
-}
-
-//this function clears the high scores
-clearBtn.addEventListener("click", function () {
-    localStorage.clear();
-    location.reload();
+//local storage for the high score page
+(highScores = JSON.parse(localStorage.getItem("highScores") || "[]")),
+  (highScoreList = document.getElementById("highScoreList"));
+highScores.sort(function(low, high) {
+  return high.score - low.score;
 });
 
+//this function makes and sorts the high scores
+window.addEventListener("load", function() {
+  html = "<table><tr>";
+  for (var scoreNum = 0; scoreNum < highScores.length; scoreNum++) {
+    html +=
+      "<td>" +
+      highScores[scoreNum].name +
+      "</td>" +
+      "<td>" +
+      highScores[scoreNum].score +
+      "</td>";
+  }
+  html += "</tr></table>";
+  document.getElementById("highScoreList").innerHTML = html;
+});
+
+//this function clears the high scores
+clearBtn.addEventListener("click", clearScores);
+startBtn.addEventListener("click", startTimer);
+
+function clearScores() {
+  localStorage.clear();
+  location.reload();
+}
--- main.js
+++ main.js
@@ -1,107 +1,105 @@
-//various variables
+//The three main pages linked on the index.html page
+var quizPage = document.getElementById("quiz");
+var homePage = document.getElementById("homePage");
+var finalPage = document.getElementById("submit-score");
+//misc. variables
+var question = document.getElementById("questions");
+var answers = document.getElementById("answers");
 var startBtn = document.getElementById("startBtn");
 var submitBtn = document.getElementById("submitBtn");
-var secondsLeft = (90);
-var timerEl = document.getElementById("timer");
-var questionHead = document.getElementById("questions");
-var answerChoices = document.getElementById("answers");
-var answerEl = document.getElementById("answers");
-var correctAnswers = (0);
-var questionNumber = -1;
-var answer = document.getElementById("answer");
+var secondsLeft = 90;
+var quizTimer = document.getElementById("timer");
+var correctAnswers = 0;
+var questionNumber = 0;
 
-//function to start the timer when the start button is pressed
-function startTimer() {
-    document.getElementById("homePage").classList.add("d-none");
-    document.getElementById("quiz").classList.remove("d-none");
-    setTimer();
-    renderQuestions();
-}
+//on load screen the quiz page and the final page will be hidden will be hidden
+window.onload = function() {
+  quizPage.hidden = true;
+  homePage.hidden = false;
+  finalPage.hidden = true;
+  quizTimer.hidden = true;
+};
 
-//function to set the timer to 90 seconds and to make it so the timer goes in seconds to zero
-function setTimer() {
-    var countdown = setInterval(function () {
-        secondsLeft--;
-        timer.textContent = "time left:" + secondsLeft;
-
-        if (secondsLeft === 0) {
-            clearInterval(countdown);
-        }
-    }, 1000);
-}
-
-//function makes questions appear with four answers in blocks
-function renderQuestions() {
-    questionNumber++;
-
-    if (questionNumber > 9) {
-        alert("Nice job, that was the last question");
-        endQuiz();
-        return;
-    }
-
-    if (secondsLeft <= 0) {
-        alert("Sorry you have run out of time");
-        endQuiz();
-        return;
-    }
-
-    ;
-    answer = questions[questionNumber].answer
-
-    questionHead.textContent = questions[questionNumber].title;
-    answerChoices.innerHTML = "";
-
-    var choices = questions[questionNumber].choices;
-
-
-    for (var i = 0; i < choices.length; i++) {
-        var nextChoice = document.createElement("button");
-
-        nextChoice.textContent = choices[i]
-        answerBtn = answerChoices.appendChild(nextChoice).setAttribute("class", "btn btn-light btn-block")
-    }
-
-}
-
-function endQuiz() {
-    document.getElementById("quiz").classList.add("d-none");
-    document.getElementById("submit-score").classList.remove("d-none");
-}
-
-function submitScore() {
-    userNameInput = document.getElementById("userName").value;
-
-    var newScore = {
-        name: userNameInput,
-        score: correctAnswers
-    };
-
-
-    var highScores = JSON.parse(localStorage.getItem("highScores") || "[]");
-
-    highScores.push(newScore);
-
-    localStorage.setItem("highScores", JSON.stringify(highScores));
-
-}
-
-//two buttons need click events
+//various buttons
 startBtn.addEventListener("click", startTimer);
 submitBtn.addEventListener("click", submitScore);
+answers.addEventListener("click", selectAnswer);
 
-//answer buttons that needed click events
-answerChoices.addEventListener("click", function (event) {
+//function to start the timer
+function startTimer() {
+  quizPage.hidden = false;
+  homePage.hidden = true;
+  setTimer();
+  renderQuestions();
+}
 
-    //this is somewhere i would like to work on i couldnt figure out how to get correct with out alerting correct
-    if (answer === event.target.textContent) {
-        alert("Correct!");
-        correctAnswers++;
-        setTimeout(1000);
-    } else {
-        alert("Sorry, that's incorrect.");
-        setTimeout(1000);
-        secondsLeft = secondsLeft - 10;
-    }
-    renderQuestions();
-});
+//function to renderQuestions if question number is less than ten and they have time left
+function renderQuestions() {
+  if (questionNumber > 9) {
+    alert("Nice job, that was the last question");
+    endQuiz();
+    return;
+  }
+  if (secondsLeft <= 0) {
+    alert("Sorry you have run out of time");
+    endQuiz();
+    return;
+  }
+
+  answer = questions[questionNumber].answer;
+
+  question.textContent = questions[questionNumber].title;
+
+  answers.innerHTML = "";
+
+  var choices = questions[questionNumber].choices;
+
+  for (var i = 0; i < choices.length; i++) {
+    var option = document.createElement("button");
+    option.textContent = choices[i];
+    answerBtn = answers
+      .appendChild(option)
+      .setAttribute("class", "btn btn-block");
+  }
+
+  questionNumber++;
+}
+
+function setTimer() {
+  setInterval(function() {
+    secondsLeft--;
+    timer.textContent = "time left:" + secondsLeft;
+    quizTimer.hidden = false;
+  }, 1000);
+}
+
+function selectAnswer() {
+  //this is somewhere i would like to work on i couldnt figure out how to get correct with out alerting correct
+  if (answer === event.target.textContent) {
+    alert("Correct!");
+    correctAnswers++;
+  } else {
+    alert("Incorrect!");
+    secondsLeft = secondsLeft - 10;
+  }
+  renderQuestions();
+}
+
+//this function leads to the final submit page
+function endQuiz() {
+  quizPage.hidden = true;
+  finalPage.hidden = false;
+  quizTimer.hidden = true;
+}
+
+//function that takes the username submission keeps it in local storage and sends it to high score page
+function submitScore() {
+  userName = document.getElementById("userName").value;
+  var newScore = {
+    name: userName,
+    score: correctAnswers
+  };
+  var highScores = JSON.parse(localStorage.getItem("highScores"));
+  highScores.push(newScore);
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+}
